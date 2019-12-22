27f2257039c4a63b15faf024909cd5aba2b4af21
Muhammad Ali Mazhuda <m.alli9401@gmail.com>
2019-12-22 09:46:58 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-showMenu = () => {
-    let id = document.getElementById('nav');
-    if (id.className === 'nav-bar') {
-        id.className += ' responsive';
-    } else {
-        id.className = 'nav-bar';
-    }
-    console.log(id.className);
-}
